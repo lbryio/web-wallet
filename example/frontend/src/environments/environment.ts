8b28bd35ffa69a7180d335d57c305caebd882e1b
Daniel Krol <orblivion@gmail.com>
2022-04-09 16:07:30 -0400

    Delete a bunhc of environment vars we don't care about.

--- example/frontend/src/environments/environment.ts
+++ example/frontend/src/environments/environment.ts
@@ -5,21 +5,5 @@
 export const environment = {
   production: false,
   backendHostname: 'localhost:8090',
-  uploadImageHostname: "node.deso.org",
-  verificationEndpointHostname: "https://node.deso.org",
-  uploadVideoHostname: "node.deso.org",
-  identityURL: "https://identity.deso.org",
-  supportEmail: "",
-  dd: {
-    apiKey: "DCEB26AC8BF47F1D7B4D87440EDCA6",
-  },
-  amplitude: {
-    key: "",
-    domain: "",
-  },
-  node: {
-    id: 1,
-    name: "DeSo",
-    url: "https://node.deso.org",
-  },
+  identityURL: "http://localhost:4201",
 };
