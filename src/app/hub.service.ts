d0b197c5b093e708f3df9ff18235f1b47fdedd34
Daniel Krol <orblivion@gmail.com>
2022-05-10 14:59:39 -0400

    Begin to get Hub service ready for http connections

--- src/app/hub.service.ts
+++ src/app/hub.service.ts
@@ -1,12 +1,19 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HubService {
+  hubEndpoint = `https://${environment.hubHostname}/api/v0`;
 
   constructor(
+    private httpClient: HttpClient,
   ) { }
+
+  post(path: string, body: any): Observable<any> {
+    return this.httpClient.post<any>(`${this.hubEndpoint}/${path}`, body);
+  }
 
   // Just for stubbing until we get the real thing
   private rndStr() {
@@ -15,6 +22,7 @@ export class HubService {
 
   // Obviously just a stub for the actual API
   public getChannels(xPubs: string[]): any {
+
     return [{
       claimId: this.rndStr(),
       handle: '@test-' + this.rndStr(),
